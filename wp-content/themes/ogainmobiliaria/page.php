699e09990b666d076939d42aada9b7fbe42b7250
max <maxwellweb@gmail.com>
2016-02-18 17:18:56 -0300

    Tema base

---
+++ wp-content/themes/ogainmobiliaria/page.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package ogainmobiliaria
+ */
+
+get_header(); ?>
+<section class="cats-top">
+  <div class="wrapper">
+    <div class="row">
+      <div class="col col--9-of-12 col--centered">
+        <h2><span>[</span>Propiedades en venta<span>]</span></h2>
+      </div>
+    </div>
+  </div>
+</section>
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php
+			while ( have_posts() ) : the_post();
+
+				get_template_part( 'template-parts/content', 'page' );
+
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+
+			endwhile; // End of the loop.
+			?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+//get_sidebar();
+get_footer();
