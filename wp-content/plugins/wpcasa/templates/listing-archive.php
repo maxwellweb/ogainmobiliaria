5e1904fe6c479c0afa800811ce4b029bf29581a2
max <maxwellweb@gmail.com>
2016-02-22 19:18:46 -0300

    muestra de salida a imprimir..

--- wp-content/plugins/wpcasa/templates/listing-archive.php
+++ wp-content/plugins/wpcasa/templates/listing-archive.php
@@ -31,3 +31,42 @@
 	</div>
 
 </div><!-- #listing-<?php the_ID(); ?> -->
+
+	 <div class="col col--6-of-12 col--m-2-of-4">
+        <div class="propertie sell">
+          <div class="propertie-img">
+            <a href="#">
+             <?php wpsight_get_template( 'listing-archive-image.php' ); ?>
+            </a>
+            <div class="propertie-price">528.000 €</div>
+          </div><!-- IMG -->
+          <div class="propertie-info">
+            <h3>Carabanchel Alto <span>Madrid</span></h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+            <ul>
+              <li>2 <span>Aseos</span></li>
+              <li>3 <span>Dormitorios</span></li>
+              <li>352 <span>M2</span></li>
+            </ul>
+          </div><!-- INFO -->
+        </div>
+      </div>
+    <div class="col col--6-of-12 col--m-2-of-4">
+        <div class="propertie sell">
+          <div class="propertie-img">
+            <a href="#">
+              <img src="assets/img/fuenlabrada.jpg" alt="Featured Test Image">
+            </a>
+            <div class="propertie-price">613.000 €</div>
+          </div><!-- IMG -->
+          <div class="propertie-info">
+            <h3>Carabanchel Alto <span>Madrid</span></h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</p>
+            <ul>
+              <li>2 <span>Aseos</span></li>
+              <li>3 <span>Dormitorios</span></li>
+              <li>352 <span>M2</span></li>
+            </ul>
+          </div><!-- INFO -->
+        </div>
+      </div><!-- PROPERTIE -- SELL -->
