d2967ad305cedc5172be45ffae90f2f2391d18fd
laura villalba <lauchri14@gmail.com>
2016-02-25 15:04:49 -0300

    cambios faltantes

--- wp-config.php
+++ wp-config.php
@@ -22,7 +22,7 @@ define('DB_NAME', 'ogainmo');
 define('DB_USER', 'root');
 
 /** Tu contraseña de MySQL */
-define('DB_PASSWORD', 'root');
+define('DB_PASSWORD', 'sofiaxiomara');
 
 /** Host de MySQL (es muy probable que no necesites cambiarlo) */
 define('DB_HOST', 'localhost');
