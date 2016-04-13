94c593a1c9fa6122ff1ec5d7629d34502bbefa59
Seba Torresi <sebatorresi@gmail.com>
2016-04-12 20:07:18 -0400

    Arrelando la tipografía
    Ya la tipografía es legible, le cambié el “weight”

--- wp-config.php
+++ wp-config.php
@@ -22,7 +22,7 @@ define('DB_NAME', 'ogainmo');
 define('DB_USER', 'root');
 
 /** Tu contraseña de MySQL */
-define('DB_PASSWORD', 'sofiaxiomara');
+define('DB_PASSWORD', 'mysql');
 
 /** Host de MySQL (es muy probable que no necesites cambiarlo) */
 define('DB_HOST', 'localhost');
