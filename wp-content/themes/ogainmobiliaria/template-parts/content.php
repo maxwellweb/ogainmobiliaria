69ac516798443db262c9fa36afcb4962d89a80ee
Seba Torresi <sebatorresi@gmail.com>
2016-04-17 20:48:38 -0400

    traducción

--- wp-content/themes/ogainmobiliaria/template-parts/content.php
+++ wp-content/themes/ogainmobiliaria/template-parts/content.php
@@ -29,7 +29,7 @@
 			<?php
 				the_content( sprintf(
 					/* translators: %s: Name of current post. */
-					wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'ogainmobiliaria' ), array( 'span' => array( 'class' => array() ) ) ),
+					wp_kses( __( 'Continúa leyendo %s <span class="meta-nav">&rarr;</span>', 'ogainmobiliaria' ), array( 'span' => array( 'class' => array() ) ) ),
 					the_title( '<span class="screen-reader-text">"', '"</span>', false )
 				) );
 
